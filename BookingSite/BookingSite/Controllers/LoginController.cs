b74ac0524efc611361681fc505b247a7579e0e84
ElectricCoffee <slench102+git@gmail.com>
2013-11-20 19:37:19 +0100

    Påbegyndt loginsystem

---
+++ BookingSite/BookingSite/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Grimt Loginsystem";
+            return View();
+        }
+
+        // POST: /Login/Index
+
+        [HttpPost, ActionName("Do")]
+        public ActionResult Login()
+        {
+            TempData["Username"] = Request.Form["usernameBox"];
+            TempData["Password"] = Request.Form["passwordBox"];
+
+            return new RedirectResult("/MemberPage/Index");
+        }
+
+    }
+}
---
+++ BookingSite/BookingSite/Controllers/MemberPageController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingSite.Controllers
+{
+    public class MemberPageController : Controller
+    {
+        //
+        // GET: /MemberPage/
+
+        public ActionResult Index()
+        {
+            ViewBag.Username = TempData["Username"];
+            ViewBag.Password = TempData["Password"];
+            
+
+            return View();
+        }
+
+        
+    }
+}
