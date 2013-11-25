41a41cbc0731f9653c0ba9701ac1c2cac6e96ca9
Mick Lindhardsen <mvlindhardsen@gmail.com>
2013-11-25 15:48:18 +0100

    Påbegynt opret studerende

---
+++ BookingSite/BookingSite/Controllers/RegisterUserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingSite.Controllers
+{
+    public class RegisterUserController : Controller
+    {
+        //
+        // GET: /RegisterUser/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Do")]
+
+        public ActionResult Register()
+        {
+            var uri = string.Format("http://localhost:14781/api/");
+        }
+    }
+}
