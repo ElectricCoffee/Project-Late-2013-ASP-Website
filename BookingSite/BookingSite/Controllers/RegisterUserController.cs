c5d88f5ebe87cc88ed05cbc6cc24371a6fa979f1
Mick Lindhardsen <mvlindhardsen@gmail.com>
2013-11-26 15:12:40 +0100

    Tilføjet css

--- BookingSite/BookingSite/Controllers/RegisterUserController.cs
+++ BookingSite/BookingSite/Controllers/RegisterUserController.cs
@@ -15,33 +15,47 @@ namespace BookingSite.Controllers
 
         public ActionResult Index()
         {
+            ViewBag.Title = "Registrer ny bruger";
             return View();
         }
 
+        /// <summary>
+        /// Gets a response from the rest server based on the user's credentials, and returns an error or a positive message accordingly
+        /// </summary>
+        /// <returns></returns>
         [HttpPost, ActionName("Do")]
-
         public ActionResult Register()
         {
+            Action<String> message = str => TempData["Message"] = str;
+
             ///*
+            // These are the values gotten from the input fields on the web-page.
             var firstname = Request.Form["Firstname"];
             var lastname = Request.Form["Lastname"];
             var email = Request.Form["Email"];
+            var repeat = Request.Form["Repeat"];
             var password = Request.Form["Password"];
             var homeroom = Request.Form["Homeroom"];
 
-            var uri = string.Format(
-                "http://localhost:14781/api/registeruser?firstname={0}&lastname={1}&email={2}&password={3}&homeroomClass={4}", 
-                firstname, lastname, email, password, homeroom);
-
-            var response = ServerCommunicator.Get(uri).DeserializeJson<RestResponseContainer>();
-
-            if (response.Key.Contains("Error"))
+            // if the password and the repeated password don't match, then you aren't allowed to even connect to the REST server.
+            if (password.Equals(repeat))
             {
-                TempData["Message"] = "Der kunne desværre ikke blive oprettet en bruger i systemet," +
-                    " da de indtastede oplysninger var forkerte, vær venlig at prøve igen";
+
+                var uri = string.Format(
+                    "http://localhost:14781/api/registeruser?firstname={0}&lastname={1}&email={2}&password={3}&homeroomClass={4}",
+                    firstname, lastname, email, password, homeroom);
+
+                var response = ServerCommunicator.Get(uri).DeserializeJson<RestResponseContainer>();
+
+                if (response.Key.Contains("Error"))
+                {
+                    message("Der kunne desværre ikke blive oprettet en bruger i systemet," +
+                        " da de indtastede oplysninger var forkerte, vær venlig at prøve igen");
+                }
+                else message("Tillykke, du er nu oprettet, vent på at din lærer har godkendt det.");
+                //*/
             }
-            else TempData["Message"] = "Tillykke, du er nu oprettet, vent på at din lærer har godkendt det.";
-            //*/
+            else message("Du har tastet din adgangskode forkert, prøv igen.");
 
             return new RedirectResult("/RegisterUser/ResponsePage");
         }
