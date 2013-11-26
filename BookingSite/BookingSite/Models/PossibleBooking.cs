73ed331b952858f0a83679dbbff8f68636d3bb56
Mick Lindhardsen <mvlindhardsen@gmail.com>
2013-11-27 00:39:58 +0100

    Bookingsite påbegyndt

---
+++ BookingSite/BookingSite/Models/PossibleBooking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookingSite.Models
+{
+    public class PossibleBookingList
+    {
+        public List<PossibleBooking> Bookings { get; set; }
+
+        public PossibleBookingList()
+        {
+            Bookings = new List<PossibleBooking>();
+        }
+    }
+
+    public class PossibleBooking
+    {
+        public string Teacher { get; set; }
+        public string Subject { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
