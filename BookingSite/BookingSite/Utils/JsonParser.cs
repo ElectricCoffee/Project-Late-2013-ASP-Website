2333ce30cb92bc546b836604828675ed2845fb33
Niko Lepka <slench102+git@gmail.com>
2013-12-04 16:57:54 +0100

    updated json serializer

--- BookingSite/BookingSite/Utils/JsonParser.cs
+++ BookingSite/BookingSite/Utils/JsonParser.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Script.Serialization;
 
-namespace BookingSite.Utils
+namespace REST_Service.Utils
 {
-    public static class JsonParser
+    /// <summary>
+    /// Serves to remove boilerplate code, by simply letting the user call a method directly on the object you're using
+    /// </summary>
+    public static class JsonConverter
     {
         /// <summary>
         /// Takes any JSON Object in string form, and deserializes it to the specified class
@@ -17,7 +19,8 @@ namespace BookingSite.Utils
         /// <returns>An Object that contains the data</returns>
         public static T DeserializeJson<T>(this string input)
         {
-            return new JavaScriptSerializer().Deserialize<T>(input);
+            // return new JavaScriptSerializer().Deserialize<T>(input);
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(input);
         }
 
         /// <summary>
@@ -28,9 +31,8 @@ namespace BookingSite.Utils
         /// <returns>String containing a JSON Object</returns>
         public static string SerializeToJsonObject<T>(this T input)
         {
-            string value = new JavaScriptSerializer().Serialize(input);
-            Debug.WriteLine(value);
-            return value;
+            return Newtonsoft.Json.JsonConvert.SerializeObject(input);
+            //return new JavaScriptSerializer().Serialize(input);
         }
     }
 }
