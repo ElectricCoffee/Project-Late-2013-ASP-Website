1a91b6cf10ab20191074332945cbde8bbc6751eb
TrineThune <thune89@gmail.com>
2013-12-10 14:46:53 +0100

    DailyRepport HTML

---
+++ BookingSite/BookingSite/Controllers/DailyRepportController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingSite.Controllers
+{
+    public class DailyRepportController : Controller
+    {
+        //
+        // GET: /DailyRepport/
+
+        public ActionResult Index()
+        {
+            List<DateTime> dates = new List<DateTime>();
+            dates.Add(DateTime.Now);
+            ViewBag.Dates = dates;
+            return View();
+        }
+
+    }
+}
