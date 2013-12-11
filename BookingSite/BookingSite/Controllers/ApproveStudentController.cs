aa38c4b3df0c898e9a1b284696d17e4a07de2788
Mick Lindhardsen <mvlindhardsen@gmail.com>
2013-12-11 13:22:22 +0100

    HTML godkendelse af studerende

---
+++ BookingSite/BookingSite/Controllers/ApproveStudentController.cs
@@ -0,0 +1,32 @@
+﻿using BookingSite.Models;
+using BookingSite.Utils;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingSite.Controllers
+{
+    public class ApproveStudentController : Controller
+    {
+        private const string SERVER_URI = "http://localhost:14781/api/";
+
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student
+                {
+                    Name = new Name
+                        {
+                            FirstName = "Trine",
+                            LastName = "Thune"
+                        }
+                });
+            ViewBag.Students = students;
+
+            return View();
+        }
+    }
+}
